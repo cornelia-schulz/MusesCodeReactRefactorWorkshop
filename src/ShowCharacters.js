41f428c0943452f484a5698c4a450a1f36545418
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2019-11-16 15:21:21 +1100

    refactor Charactor into its own component

--- src/ShowCharacters.js
+++ src/ShowCharacters.js
@@ -1,29 +1,6 @@
 import React, { Component } from 'react';
-import Img from './Img';
+import Character from './Character';
 const groupSize = 3;
-
-const Character = (props) => (
-  <div className="card text-white bg-primary mb-3"
-      style={{maxWidth: "20rem"}}
-      data-testid="character">
-    <div className="card-body">
-      <h4 className="card-title" data-testid="name">{props.character.name}</h4>
-        <Img
-          name={props.character.name}
-          src={props.character.thumbnail.path+"."+props.character.thumbnail.extension}
-        />
-      <p className="card-text" data-testid="descr">{props.character.description}</p>
-      <button
-        className="btn btn-outline-secondary"
-        data-testid="deleteButton"
-        data-id={props.character.id} 
-        onClick={props.removeCharacter}
-      >
-        Delete
-      </button>
-    </div>
-  </div>
-);
 
 class ShowCharacters extends Component{
   constructor(){
